cccd0398a895593a0e0262f43651ee6324998e0c
Valentin <7705678@gmail.com>
2022-04-01 00:42:45 +0300

    .

--- 14-primes/script.js
+++ 14-primes/script.js
@@ -33,3 +33,19 @@ function primes(num) {
 
 console.log(primes(6)); // [2, 3, 5]
 //console.log(primes(17)); // [2, 3, 5, 7, 11, 13, 17]
+
+
+
+const values = [56, 78, 100],
+      sum = 7777,
+      result = [];
+let temp = 0;
+
+result[2] = Math.trunc(sum / values[2]);
+temp = sum - (result[2] * values[2]);
+result[1] = Math.trunc(temp / values[1]);
+temp = temp - (result[1] * values[1]);
+result[0] = Math.trunc(temp / values[0]);
+console.log(result);
+
+console.assert(add(3,4) === 7, 'sdgdfgdfg');
